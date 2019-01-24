c9deb6f357d9ac0b0feb3039bc257f4728f5129b
MHorwell <mattjhorwell@gmail.com>
2019-01-24 14:13:28 +0000

    Tests are now stored in one report

--- src/test/java/com/horwell/matthew/tea/tasting/website/test/TeaSiteSteps.java
+++ src/test/java/com/horwell/matthew/tea/tasting/website/test/TeaSiteSteps.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
@@ -17,8 +16,8 @@ import cucumber.api.java.en.When;
 
 import com.horwell.matthew.tea.tasting.website.LandingPage;
 import com.horwell.matthew.tea.tasting.website.MenuPage;
-import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class TeaSiteSteps {
 
@@ -30,40 +29,44 @@ public class TeaSiteSteps {
 	ExtentTest test;
 
 	WebDriver driver;
-	ExtentReports report = new ExtentReports(Constant.REPORTPATH + Constant.REPORTFILE, false);
-	
+		
 
 	@Before
 	public void setup() {
 		System.setProperty("webdriver.gecko.driver", Constant.FIREFOXDRIVERPATH + Constant.FIREFOXDRIVER);
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
-		test = report.startTest("Test");
+		test = TestRunner.report.startTest("Test");
 	}
 
 	@After
 	public void tearDown() {
-		report.endTest(test);
 		driver.quit();
-		report.flush();
 	}
 
 	@Given("^the correct web address$")
 	public void the_correct_web_address() {
 		driver.get(Constant.URL);
 		landingPage = PageFactory.initElements(driver, LandingPage.class);
+		test.log(LogStatus.INFO, "Landing page loaded");
 	}
 
 	@When("^I navigate to the 'Menu' page$")
 	public void i_navigate_to_the_Menu_page() {
 		landingPage.goToMenu();
 		menuPage = PageFactory.initElements(driver, MenuPage.class);
+		test.log(LogStatus.INFO, "Menu page loaded");
 	}
 
 	@Then("^I can browse a list of the available products\\.$")
 	public void i_can_browse_a_list_of_the_available_products() {
-
+		if (menuPage.getTeaList().equals("Green Tea, Red Tea, Oolong Tea")) {
+			test.log(LogStatus.PASS, "All teas are listed");
+		} else {
+			test.log(LogStatus.PASS, "Teas aren't listed");
+		}
 		assertEquals("Green Tea, Red Tea, Oolong Tea", menuPage.getTeaList());
+		TestRunner.report.endTest(test);
 	}
 
 	@When("^I click the checkout button$")
@@ -73,7 +76,14 @@ public class TeaSiteSteps {
 
 	@Then("^I am taken to the checkout page$")
 	public void i_am_taken_to_the_checkout_page() {
-		assertEquals("http://www.practiceselenium.com/check-out.html", driver.getCurrentUrl());
+		String currentURL = driver.getCurrentUrl();
+		if (currentURL.equals("http://www.practiceselenium.com/check-out.html")) {
+			test.log(LogStatus.PASS, "Checkout page loaded successfully");
+		} else {
+			test.log(LogStatus.PASS, "At incorrect URL");
+		}
+		assertEquals("http://www.practiceselenium.com/check-out.html", currentURL);
+		TestRunner.report.endTest(test);
 	}
 	
 
---
+++ src/test/java/com/horwell/matthew/tea/tasting/website/test/TestRunner.java
@@ -0,0 +1,28 @@
+package com.horwell.matthew.tea.tasting.website.test;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import com.relevantcodes.extentreports.ExtentReports;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:")
+public class TestRunner {
+	
+	static ExtentReports report;
+	
+	@BeforeClass
+	public static void startReport(){
+		report = new ExtentReports(Constant.REPORTPATH + Constant.REPORTFILE, true);
+	}
+	
+	@AfterClass
+	public static void finishReport() {
+		report.flush();
+	}
+	
+}
